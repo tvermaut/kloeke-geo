ecf2f9d30f010d431d76015e2e6811df83df8669
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2021-03-04 18:19:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ var kaart = L.map('kaart').setView({lon: 52, lat: 5}, 8);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: 'Team Geo @ HuC, KNAW'
-}).addTo(map);
+}).addTo(kaart);
